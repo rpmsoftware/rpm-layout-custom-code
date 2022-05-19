9fa7b90efbee96a6eb77dbd9abac34c83280bc35
o-wu <87500503+o-wu@users.noreply.github.com>
2022-05-19 11:10:17 -0600

    Create DisplayTimeOnly.js

---
+++ HTML-CSS Examples/Display Time Only/DisplayTimeOnly.js
@@ -0,0 +1,16 @@
+
+<span class="only-time">[Time 'all clear given':]</span>
+
+//Add the following script to the bottom of your layout.
+
+<script>
+	$$('body').addEvent('RpmLayoutRendered', function() {
+    	$$('.only-time').each(function(el) {
+    		var date = el.get('text').toDate();
+    		if (date === null) {
+    			return;
+    		}
+    		el.set('text', date.getNiceTime());
+    	});
+	});
+</script>
